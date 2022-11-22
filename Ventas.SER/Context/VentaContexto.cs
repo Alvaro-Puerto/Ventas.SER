2c3bffeb90ec627b82dd30077a1aa1675083e54d
AlvaroPuerto <alvaroevenor.puertotellez@gmail.com>
2022-11-22 11:45:50 -0600

    Agregando conexion a BD y migraciones iniciales

---
+++ Ventas.SER/Context/VentaContexto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ventas.SER.Context
+{
+    public class VentaContexto : DbContext
+    {
+        public VentaContexto(DbContextOptions<VentaContexto> opcional) : base(opcional)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- Ventas.SER/Program.cs
+++ Ventas.SER/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Ventas.SER.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Conexion Base de datos
+builder.Services.AddDbContext<VentaContexto>(opciones =>
+        opciones.UseSqlServer(builder.Configuration.GetConnectionString("ConexionDB"))
+    );
 
 var app = builder.Build();
 
